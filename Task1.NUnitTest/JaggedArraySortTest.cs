8bd66cb29f5c4efa93fee42be36f860b56f6a74f
Vadim Gatsura <vadimgatsura@gmail.com>
2015-10-25 19:25:56 +0300

    Complete Task1

---
+++ Task1.NUnitTest/JaggedArraySortTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task1.NUnitTest {
+
+    [TestFixture]
+    public class JaggedArraySortTest {
+        //RowSum: 16, 0, null, null, 27, 26
+        //MaxAbsElement: 10, 0, null, null, 8, 95
+        readonly int[][] m_Array = { new[] { 4, 2, -1, 9, -8, 10 }, new[] { 0 }, null, null, new[] { 4, 3, 8, 5, 7 }, new []{45, 7, -95, 21, -1, 0, 37, 12} };
+
+        public IEnumerable<TestCaseData>TestDatas {
+            get {
+                yield return new TestCaseData(new[] {new [] {0}, new[] { 4, 2, -1, 9, -8, 10 }, new[] { 45, 7, -95, 21, -1, 0, 37, 12 }, new[] { 4, 3, 8, 5, 7 }, null, null}, new SumArrayRowComparator(), true);
+                yield return new TestCaseData(new[] { new[] { 0 }, new[] { 4, 3, 8, 5, 7 }, new[] { 4, 2, -1, 9, -8, 10 }, new[] { 45, 7, -95, 21, -1, 0, 37, 12 }, null, null }, new MaxAbsArrayElementComparator(), true);
+
+                yield return new TestCaseData(new[] { null, null, new[] { 4, 3, 8, 5, 7 }, new[] { 45, 7, -95, 21, -1, 0, 37, 12 }, new[] { 4, 2, -1, 9, -8, 10 }, new[] { 0 } }, new SumArrayRowComparator(), false);
+                yield return new TestCaseData(new[] {null, null, new[] { 45, 7, -95, 21, -1, 0, 37, 12 }, new[] { 4, 2, -1, 9, -8, 10 }, new[] { 4, 3, 8, 5, 7 }, new[] { 0 } }, new MaxAbsArrayElementComparator(), false);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestDatas))]
+        public void SortArray_Test(int[][] sortedArray, IArrayComporator<int> comparator, bool asc) {
+
+            int[][] expectedArray = JaggedArraySort.Sort(m_Array, comparator);
+            if(!asc)
+                expectedArray = expectedArray.Reverse().ToArray();
+            
+            CollectionAssert.AreEqual(expectedArray, sortedArray);
+        }
+
+    }
+}
---
+++ Task1/JaggedArraySort.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Task1 {
+
+    /// <summary>
+    /// Interface for compare two arrays
+    /// </summary>
+    /// <typeparam name="T">The type of array elements</typeparam>
+    public interface IArrayComporator<T>  {
+        /// <summary>
+        /// Compare two arrays
+        /// </summary>
+        /// <param name="array1">The first array</param>
+        /// <param name="array2">The second array</param>
+        /// <returns>The comparison result</returns>
+        int Compare(T[] array1, T[] array2);
+    } 
+
+    /// <summary>
+    /// Sorting the jagged array
+    /// </summary>
+    public static class JaggedArraySort {
+        #region Public Methods 
+
+        /// <summary>
+        /// Sort jagged array by the <see cref="IArrayComporator"/>
+        ///  <remarks>null sub-arrays go to the end of jagged array</remarks> 
+        /// </summary>
+        /// <typeparam name="T">The type of array elements</typeparam>
+        /// <param name="array">Jagged array for sorting</param>
+        /// <param name="comparator">Interface, which allow compare two sub-arrays</param>
+        public static T[][] Sort<T>(T[][] array, IArrayComporator<T> comparator) {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+            if (comparator == null)
+                throw new ArgumentNullException(nameof(comparator));
+
+            T[][] tempArray = CopyArray(array);
+            SortArray(tempArray, comparator); 
+            return tempArray;
+        }
+        #endregion
+
+        #region Private Methods
+        private static void Swap<T>(ref T[] firstArray, ref T[] secondArray) {
+            var bufferArray = firstArray;
+            firstArray = secondArray;
+            secondArray = bufferArray;
+        }
+
+        private static T[][] CopyArray<T>(T[][] array) {
+            T[][] tempArray = new T[array.Length][];
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] == null) {
+                    tempArray[i] = null;
+                    continue;
+                }
+                tempArray[i] = new T[array[i].Length];
+                for (int j = 0; j < array[i].Length; j++) {
+                    tempArray[i][j] = array[i][j];
+                }
+
+            }
+            return tempArray;
+        }
+
+        private static void SortArray<T>(T[][] array, IArrayComporator<T> comparator) {
+            for (int i = array.Length - 1; i > 0; i--) {
+                for (int j = 0; j < i; j++) {
+                    if (comparator.Compare(array[j], array[j + 1]) > 0) {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        } 
+        #endregion
+    }
+}
